156c480c8b9428b884b68b89a2a571450a398f78
João Matheus Filgueira Serafim <joaomatheusserafim@gmail.com>
2025-10-30 01:01:08 -0300

    Add missing frontend components (InputText, Button, Checkbox) and Register.css; finalize merge fixes

---
+++ frontend/src/Components/Button/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function ActionButton({ text, onClick, style = {}, type = "button" }) {
+  return (
+    <button className="action-button" onClick={onClick} style={style} type={type}>
+      <h3>{text}</h3>
+    </button>
+  );
+}
+
+export default ActionButton;
